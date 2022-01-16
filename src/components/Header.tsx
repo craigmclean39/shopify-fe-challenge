f009edaca1871725449d08c542516555d3ddbde2
Craig McLean <craig.mclean39@gmail.com>
2022-01-16 11:14:48 -0800

    Add NASA and Shopify links to header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 import Spacestagram from '../media/spacestagram.png';
+import { ReactComponent as NasaIcon } from '../media/nasa.svg';
+import { ReactComponent as ShopifyIcon } from '../media/shopify.svg';
 
 const Header = () => {
   return (
     <header>
       <div className='inner-header'>
         <img src={Spacestagram} alt='' />
+        <div className='link-icons'>
+          <a href='https://www.nasa.gov' target='_blank' rel='noreferrer'>
+            <NasaIcon className='header-icon nasa' />
+          </a>
+          <a href='https://www.shopify.com' target='_blank' rel='noreferrer'>
+            <ShopifyIcon className='header-icon shopify' />
+          </a>
+        </div>
       </div>
     </header>
   );
